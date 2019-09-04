2fbf7f4395fe7fc7ed0f1d3aeb59c333c3e81e8d
Wikan Haryo <49051441+fakeDataScientist@users.noreply.github.com>
2019-09-04 13:00:27 +0700

    Update FindAgeApp.py

--- FindAgeApp.py
+++ FindAgeApp.py
@@ -1,4 +1,16 @@
 import datetime
-DOB=input("Enter your DOB:")
+#input month of birth and year of birth
+MOB=input("Enter your Month of Birth:")
+YOB=input("Enter your Year of Birth:")
+#define what month and year today is
+CurrentMonth=datetime.datetime.now().month
 CurrentYear=datetime.datetime.now().year
-Age=CurrentYear-int(DOB)
+#if clause
+if int(MOB)>CurrentMonth:
+    year=CurrentYear-int(YOB)-1
+    month=12-(int(MOB)-CurrentMonth)
+else:
+    year=CurrentYear-int(YOB)
+    month=CurrentMonth-int(MOB)
+print("Your age is {}".format(year))
+print("{} month".format(month))
